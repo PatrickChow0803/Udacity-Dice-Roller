58b91ecc507146196d790261d08685631b0d24b3
Patrick Chow <PatrickChow0803@gmail.com>
2019-12-03 21:27:53 -0500

    Added when to know which dice image to display

--- app/src/main/java/com/lambda/analytics/diceroller/MainActivity.kt
+++ app/src/main/java/com/lambda/analytics/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lambda.analytics.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -19,11 +20,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        var resultText:TextView = findViewById(R.id.tv_result)
-
+        
         val randomInt = Random.nextInt(6) + 1
-
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.iv_dice)
+        diceImage.setImageResource(drawableResource)
     }
 
 
